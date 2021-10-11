59621448adcc92cc1f39069d86e0daafc3c16cf5
Samweli <samweli@kartoza.com>
2021-10-11 16:49:27 +0300

    restrict user with role 'editor' to only edit datasets

--- ckanext/dalrrd_emc_dcpr/plugin.py
+++ ckanext/dalrrd_emc_dcpr/plugin.py
@@ -4,9 +4,10 @@ import ckan.plugins.toolkit as toolkit
 from .commands.test import test_ckan_cmd
 
 
-class DalrrdEmcDcprPlugin(plugins.SingletonPlugin):
+class DalrrdEmcDcprPlugin(plugins.SingletonPlugin, toolkit.DefaultDatasetForm):
     plugins.implements(plugins.IConfigurer)
     plugins.implements(plugins.IClick)
+    plugins.implements(plugins.IDatasetForm)
 
     def update_config(self, config_):
         toolkit.add_template_directory(config_, "templates")
@@ -15,3 +16,65 @@ class DalrrdEmcDcprPlugin(plugins.SingletonPlugin):
 
     def get_commands(self):
         return [test_ckan_cmd]
+
+    # IDatasetForm
+
+    def _admins_only_create(self, value, context):
+        user_name = context.get('user')
+        private = value
+        package = context.get('package')
+
+        members = toolkit.get_action('member_list')(
+            data_dict={'id': package.owner_org, 'object_type': 'user'})
+
+        admin_member_ids = [member_tuple[0] for member_tuple in members if member_tuple[2] == 'Admin']
+
+        convert_user_name_or_id_to_id = toolkit.get_converter(
+            'convert_user_name_or_id_to_id')
+        user_id = convert_user_name_or_id_to_id(user_name, context)
+
+        if (not private) and (user_id not in admin_member_ids):
+            raise toolkit.Invalid('Only Admin users may set datasets as public')
+        else:
+            return value
+
+    def _admins_only_update(self, value, context):
+
+        package = context.get('package')
+        user_name = context.get('user')
+        private = value
+
+        members = toolkit.get_action('member_list')(
+            data_dict={'id': package.owner_org, 'object_type': 'user'})
+
+        admin_member_ids = [member_tuple[0] for member_tuple in members if member_tuple[2] == 'Admin']
+
+        convert_user_name_or_id_to_id = toolkit.get_converter(
+            'convert_user_name_or_id_to_id')
+        user_id = convert_user_name_or_id_to_id(user_name, context)
+        if (not private) and (user_id not in admin_member_ids) and (private != package.private):
+            raise toolkit.Invalid('Only Admin users may set datasets as public')
+        else:
+            return value
+
+    def create_package_schema(self):
+        schema = super(DalrrdEmcDcprPlugin, self).create_package_schema()
+
+        schema.update({
+            'private': schema['private'] + [self._admins_only_create]
+        })
+        return schema
+
+    def update_package_schema(self):
+        schema = super(DalrrdEmcDcprPlugin, self).update_package_schema()
+
+        schema.update({
+            'private': schema['private'] + [self._admins_only_update]
+        })
+        return schema
+
+    def is_fallback(self):
+        return True
+
+    def package_types(self):
+        return []
