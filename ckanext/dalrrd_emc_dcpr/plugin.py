a0f58d875b29f2c5e4c08e42ed2f9787d65e7676
Samweli <samweli@kartoza.com>
2021-10-14 13:11:31 +0300

    updated the new 'editor' role logic to also apply when using ckan api

--- ckanext/dalrrd_emc_dcpr/plugin.py
+++ ckanext/dalrrd_emc_dcpr/plugin.py
@@ -1,5 +1,8 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
+from ckan.logic.validators import \
+    boolean_validator,\
+    datasets_with_no_organization_cannot_be_private
 
 from .commands.test import test_ckan_cmd
 
@@ -19,24 +22,34 @@ class DalrrdEmcDcprPlugin(plugins.SingletonPlugin, toolkit.DefaultDatasetForm):
 
     # IDatasetForm
 
-    def _admins_only_create(self, value, context):
+    def _admins_only_create(self, key, data, errors, context):
         user_name = context.get('user')
-        private = value
-        package = context.get('package')
+        private = data[key]
+
+        owner_org_list = [value
+                          for key, value in data.items()
+                          if key[0] == 'owner_org']
+        owner_org = owner_org_list[0] if owner_org_list else None
 
         members = toolkit.get_action('member_list')(
-            data_dict={'id': package.owner_org, 'object_type': 'user'})
+            data_dict={'id': owner_org, 'object_type': 'user'})
 
-        admin_member_ids = [member_tuple[0] for member_tuple in members if member_tuple[2] == 'Admin']
+        admin_member_ids = [
+            member_tuple[0]
+            for member_tuple in members
+            if member_tuple[2] == 'Admin'
+        ]
 
         convert_user_name_or_id_to_id = toolkit.get_converter(
             'convert_user_name_or_id_to_id')
         user_id = convert_user_name_or_id_to_id(user_name, context)
 
         if (not private) and (user_id not in admin_member_ids):
-            raise toolkit.Invalid('Only Admin users may set datasets as public')
+            raise toolkit.Invalid(
+                'Only Admin users may set datasets as public'
+            )
         else:
-            return value
+            return private
 
     def _admins_only_update(self, value, context):
 
@@ -47,29 +60,48 @@ class DalrrdEmcDcprPlugin(plugins.SingletonPlugin, toolkit.DefaultDatasetForm):
         members = toolkit.get_action('member_list')(
             data_dict={'id': package.owner_org, 'object_type': 'user'})
 
-        admin_member_ids = [member_tuple[0] for member_tuple in members if member_tuple[2] == 'Admin']
+        admin_member_ids = [
+            member_tuple[0]
+            for member_tuple in members
+            if member_tuple[2] == 'Admin'
+        ]
 
         convert_user_name_or_id_to_id = toolkit.get_converter(
             'convert_user_name_or_id_to_id')
         user_id = convert_user_name_or_id_to_id(user_name, context)
-        if (not private) and (user_id not in admin_member_ids) and (private != package.private):
-            raise toolkit.Invalid('Only Admin users may set datasets as public')
+        if (not private) and (user_id not in admin_member_ids):
+            raise toolkit.Invalid(
+                'Only Admin users may set datasets as public '
+                'or edit public datasets'
+            )
         else:
             return value
 
     def create_package_schema(self):
         schema = super(DalrrdEmcDcprPlugin, self).create_package_schema()
 
+        create_package_validators = [
+            boolean_validator,
+            datasets_with_no_organization_cannot_be_private,
+            self._admins_only_create
+        ]
+
         schema.update({
-            'private': schema['private'] + [self._admins_only_create]
+            'private': create_package_validators
         })
         return schema
 
     def update_package_schema(self):
         schema = super(DalrrdEmcDcprPlugin, self).update_package_schema()
 
+        update_package_validators = [
+            boolean_validator,
+            datasets_with_no_organization_cannot_be_private,
+            self._admins_only_update
+        ]
+
         schema.update({
-            'private': schema['private'] + [self._admins_only_update]
+            'private': update_package_validators
         })
         return schema
 
