3f07afcc99cca589828c0a6fa4777d0e8161672e
Samweli <samweli@kartoza.com>
2021-10-19 00:18:40 +0300

    using IAuthFunctions and IActions to restrict editor to only update and restrict publishing data

--- ckanext/dalrrd_emc_dcpr/plugin.py
+++ ckanext/dalrrd_emc_dcpr/plugin.py
@@ -1,17 +1,17 @@
+import logging
+import typing
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
-from ckan.logic.validators import (
-    boolean_validator,
-    datasets_with_no_organization_cannot_be_private,
-)
 
 from .commands.test import test_ckan_cmd
+logger = logging.getLogger(__name__)
 
 
 class DalrrdEmcDcprPlugin(plugins.SingletonPlugin, toolkit.DefaultDatasetForm):
     plugins.implements(plugins.IConfigurer)
     plugins.implements(plugins.IClick)
-    plugins.implements(plugins.IDatasetForm)
+    plugins.implements(plugins.IAuthFunctions)
+    plugins.implements(plugins.IActions)
 
     def update_config(self, config_):
         toolkit.add_template_directory(config_, "templates")
@@ -21,84 +21,82 @@ class DalrrdEmcDcprPlugin(plugins.SingletonPlugin, toolkit.DefaultDatasetForm):
     def get_commands(self):
         return [test_ckan_cmd]
 
-    # IDatasetForm
+# IAuthFunctions
 
-    def _admins_only_create(self, key, data, errors, context):
-        user_name = context.get("user")
-        private = data[key]
+    def get_auth_functions(self) -> typing.Dict[str, typing.Callable]:
+        return {
+            "package_publish": authorize_package_publish,
+        }
 
-        owner_org_list = [value for key, value in data.items() if key[0] == "owner_org"]
-        owner_org = owner_org_list[0] if owner_org_list else None
+    def get_actions(self) -> typing.Dict[str, typing.Callable]:
+        return {
+            "package_create": package_create,
+            "package_update": package_update,
+            "package_patch": package_patch,
+        }
 
-        members = toolkit.get_action("member_list")(
-            data_dict={"id": owner_org, "object_type": "user"}
-        )
 
-        admin_member_ids = [
-            member_tuple[0] for member_tuple in members if member_tuple[2] == "Admin"
-        ]
+def authorize_package_publish(
+        context: typing.Dict, data_dict: typing.Optional[typing.Dict] = None
+) -> typing.Dict[str, bool]:
+    # in here we can inspect the context and the data_dict in order to
+    # decide whether to authorize access or not
+    user_name = context.get("user")
+    owner_org = data_dict.get('owner_org') or data_dict.get('group_id')
 
-        convert_user_name_or_id_to_id = toolkit.get_converter(
-            "convert_user_name_or_id_to_id"
-        )
-        user_id = convert_user_name_or_id_to_id(user_name, context)
+    members = toolkit.get_action("member_list")(
+        data_dict={"id": owner_org, "object_type": "user"}
+    )
+    admin_member_ids = [
+        member_tuple[0] for member_tuple in members if member_tuple[2] == "Admin"
+    ]
+    convert_user_name_or_id_to_id = toolkit.get_converter(
+        "convert_user_name_or_id_to_id"
+    )
+    user_id = convert_user_name_or_id_to_id(user_name, context)
 
-        if (not private) and (user_id not in admin_member_ids):
-            raise toolkit.Invalid("Only Admin users may set datasets as public")
-        else:
-            return private
+    if user_id in admin_member_ids:
+        return {"success": True}
+    else:
+        logger.debug(f"Inside authorize_package_publish function: {locals()}")
+        return {"success": False, "msg": "You are not authorized to publish a package"}
 
-    def _admins_only_update(self, value, context):
+# IActions
 
-        package = context.get("package")
-        user_name = context.get("user")
-        private = value
 
-        members = toolkit.get_action("member_list")(
-            data_dict={"id": package.owner_org, "object_type": "user"}
-        )
+@toolkit.chained_action
+def package_create(original_action, context, data_dict):
+    """
+    Intercepts the core `package_create` action to check if package
+     is being published after being created.
+    """
+    return package_publish_check(original_action, context, data_dict)
 
-        admin_member_ids = [
-            member_tuple[0] for member_tuple in members if member_tuple[2] == "Admin"
-        ]
 
-        convert_user_name_or_id_to_id = toolkit.get_converter(
-            "convert_user_name_or_id_to_id"
-        )
-        user_id = convert_user_name_or_id_to_id(user_name, context)
-        if (not private) and (user_id not in admin_member_ids):
-            raise toolkit.Invalid(
-                "Only Admin users may set datasets as public " "or edit public datasets"
-            )
-        else:
-            return value
+@toolkit.chained_action
+def package_update(original_action, context, data_dict):
+    """
+    Intercepts the core `package_update` action to check if package is being published.
+    """
+    return package_publish_check(original_action, context, data_dict)
 
-    def create_package_schema(self):
-        schema = super(DalrrdEmcDcprPlugin, self).create_package_schema()
 
-        create_package_validators = [
-            boolean_validator,
-            datasets_with_no_organization_cannot_be_private,
-            self._admins_only_create,
-        ]
 
-        schema.update({"private": create_package_validators})
-        return schema
+@toolkit.chained_action
+def package_patch(original_action, context, data_dict):
+    """
+    Intercepts the core `package_patch` action to check if package is being published.
+    """
+    return package_publish_check(original_action, context, data_dict)
 
-    def update_package_schema(self):
-        schema = super(DalrrdEmcDcprPlugin, self).update_package_schema()
 
-        update_package_validators = [
-            boolean_validator,
-            datasets_with_no_organization_cannot_be_private,
-            self._admins_only_update,
-        ]
-
-        schema.update({"private": update_package_validators})
-        return schema
-
-    def is_fallback(self):
-        return True
-
-    def package_types(self):
-        return []
+def package_publish_check(action, context, data):
+    remains_private = toolkit.asbool(data.get("private", True))
+    result = None
+    if remains_private:
+        # make sure private state is updated, then assign result
+        data.update({"private": remains_private})
+        result = action(context, data)
+    else:
+        toolkit.check_access("package_publish", context, data)
+    return result
