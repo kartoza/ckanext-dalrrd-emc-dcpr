ff0ac215df44f80fd3b486e66b82b21b01b24704
Samweli <samweli@kartoza.com>
2021-11-12 02:55:14 +0300

    reformatted new file

--- ckanext/dalrrd_emc_dcpr/commands/init_db.py
+++ ckanext/dalrrd_emc_dcpr/commands/init_db.py
@@ -7,15 +7,16 @@ import ckan.plugins.toolkit as toolkit
 
 
 @click.command(
-    short_help=u"Adds datastore user defined in"
-               u" the ckan configurations to the datastore database.")
+    short_help="Adds datastore user defined in"
+    " the ckan configurations to the datastore database."
+)
 def create_datastore_user():
-    """ Creates user in a datastore database with read only access.
-        User credential are from the already defined ckan.datastore.read_url
-        setting in CKAN configurations.
+    """Creates user in a datastore database with read only access.
+    User credential are from the already defined ckan.datastore.read_url
+    setting in CKAN configurations.
     """
-    db = create_engine(toolkit.config['ckan.datastore.write_url'])
-    db_read_url_parts = model.parse_db_config('ckan.datastore.read_url')
+    db = create_engine(toolkit.config["ckan.datastore.write_url"])
+    db_read_url_parts = model.parse_db_config("ckan.datastore.read_url")
 
     user_name = db_read_url_parts["db_user"]
     user_password = db_read_url_parts["db_pass"]
