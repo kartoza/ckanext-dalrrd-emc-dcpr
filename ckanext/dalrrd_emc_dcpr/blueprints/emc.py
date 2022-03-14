ff48d936325cad02e16593d9ad555b0feafd1919
Ricardo Garcia Silva <ricardo@kartoza.com>
2022-03-14 17:29:04 +0000

    Creating notification for when user requests that dataset be put under maintenance

--- ckanext/dalrrd_emc_dcpr/blueprints/emc.py
+++ ckanext/dalrrd_emc_dcpr/blueprints/emc.py
@@ -1,6 +1,9 @@
 import logging
 import typing
 
+from ckan import model
+from ckan.logic.schema import default_create_activity_schema
+from ckan.plugins import toolkit
 from flask import Blueprint
 
 logger = logging.getLogger(__name__)
@@ -10,8 +13,43 @@ emc_blueprint = Blueprint(
 )
 
 
-@emc_blueprint.route("/request_package_maintenance")
-def request_package_maintenance():
+@emc_blueprint.route("/request_dataset_maintenance/<dataset_id>")
+def request_dataset_maintenance(dataset_id):
+    logger.debug(f"{locals()=}")
     logger.debug("Inside the emc request_package_maintenance view")
-    result = f"<h1>Hi from the EMC request package maintenance page!</h1>"
-    return result
+    activity_schema = default_create_activity_schema()
+
+    # this is a hacky way to relax the activity type schema validation
+    to_remove = None
+    for index, validator in enumerate(activity_schema["activity_type"]):
+        if validator.__name__ == "activity_type_exists":
+            to_remove = validator
+            break
+    if to_remove:
+        activity_schema["activity_type"].remove(to_remove)
+    to_remove = None
+    for index, validator in enumerate(activity_schema["object_id"]):
+        if validator.__name__ == "object_id_validator":
+            to_remove = validator
+            break
+    if to_remove:
+        activity_schema["object_id"].remove(to_remove)
+
+    logger.debug(f"{activity_schema=}")
+    toolkit.get_action("activity_create")(
+        context={
+            "ignore_auth": True,
+            "schema": activity_schema,
+        },
+        data_dict={
+            "user_id": toolkit.g.userobj.id,
+            "object_id": dataset_id,
+            "activity_type": "requested modification",
+            "data": None,
+        },
+    )
+
+    toolkit.h["flash_success"](
+        toolkit._("Sent notification to the dataset publishers!")
+    )
+    return toolkit.redirect_to("dataset.read", id=dataset_id)
