d63d25846c38810ddc137e78ac6d0ebf9f46c7cb
Samweli <samweli@kartoza.com>
2022-03-17 11:29:17 +0300

    added dcpr requests controller

---
+++ ckanext/dalrrd_emc_dcpr/controllers/dcpr_requests.py
@@ -0,0 +1,16 @@
+import ckan.plugins.toolkit as toolkit
+
+
+log = __import__("logging").getLogger(__name__)
+
+
+class DCPRRequestsController(toolkit.BaseController):
+    def index(self):
+        try:
+            requests = toolkit.get_action("dcpr_requests_list")({}, {})
+        except toolkit.NotAuthorized:
+            toolkit.abort(401)
+
+        return toolkit.render(
+            "dcpr_requests/index.html", extra_vars={"requests": requests}
+        )
