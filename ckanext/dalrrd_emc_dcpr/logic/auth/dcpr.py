eea1d6349a9859e7ec8f06d252705b5c7bc24d33
Samweli <samweli@kartoza.com>
2022-03-22 17:53:18 +0300

    created dcpr request read page

--- ckanext/dalrrd_emc_dcpr/logic/auth/dcpr.py
+++ ckanext/dalrrd_emc_dcpr/logic/auth/dcpr.py
@@ -34,3 +34,11 @@ def dcpr_request_list_auth(
 ) -> typing.Dict:
     logger.debug("Inside the dcpr_request_list auth")
     return {"success": True}
+
+
+@toolkit.auth_allow_anonymous_access
+def dcpr_request_show_auth(
+    context: typing.Dict, data_dict: typing.Optional[typing.Dict] = None
+) -> typing.Dict:
+    logger.debug("Inside the dcpr_request_show auth")
+    return {"success": True}
