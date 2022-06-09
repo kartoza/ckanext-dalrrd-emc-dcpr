ea3d206759f84d7b7ffd6a55a5f1c0b68528c04e
Faneva Andriamiadantsoa <fanevanjanahary@gmail.com>
2022-06-09 20:23:50 +0300

    can't remove all of dataset in rcpr request

--- ckanext/dalrrd_emc_dcpr/assets/js/removeDcprRequestDatasets.js
+++ ckanext/dalrrd_emc_dcpr/assets/js/removeDcprRequestDatasets.js
@@ -14,10 +14,21 @@ ckan.module('removeDcprRequestDatasets', function(jQuery, _){
             let indexToRemove = self.dataset['moduleIndex']
             let datasetID = '#dataset'+indexToRemove
             console.log(`Was asked to remove previous dataset, which has index ${indexToRemove}`)
-            let lastDatasetFieldset = datasetFieldsets[indexToRemove -1]
-            lastDatasetFieldset.remove()
+            let index = indexToRemove -1
+            if(datasetFieldsets.length == 1){
+                index = 0
+                let addButton = document.querySelector('#insert-dataset-fieldset-button')
+                addButton.click()
+            }
+            try {
+                datasetFieldsets[index].remove()
+            }
+            catch (e) {
+                datasetFieldsets[index - 1].remove()
+            }
             let datasetPanel = document.querySelector(datasetID)
             datasetPanel.style.display = 'none'
+
         },
     }
 
