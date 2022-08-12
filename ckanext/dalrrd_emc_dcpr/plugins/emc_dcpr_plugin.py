a1f8030008978c32ce5b6d175d48dc5f6b6ec458
Faneva Andriamiadantsoa <fanevanjanahary@gmail.com>
2022-08-12 16:50:51 +0300

    add publish page

--- ckanext/dalrrd_emc_dcpr/plugins/emc_dcpr_plugin.py
+++ ckanext/dalrrd_emc_dcpr/plugins/emc_dcpr_plugin.py
@@ -24,6 +24,7 @@ from .. import (
 from ..blueprints.dcpr import dcpr_blueprint
 from ..blueprints.emc import emc_blueprint
 from ..blueprints.xml_parser import xml_parser_blueprint
+from ..blueprints.publish import publish_blueprint
 from ..cli import commands
 from ..cli.legacy_sasdi import commands as legacy_sasdi_commands
 from ..logic.action import ckan as ckan_actions
@@ -342,6 +343,7 @@ class DalrrdEmcDcprPlugin(plugins.SingletonPlugin, toolkit.DefaultDatasetForm):
             dcpr_blueprint,
             emc_blueprint,
             xml_parser_blueprint,
+            publish_blueprint,
         ]
 
     def dataset_facets(
