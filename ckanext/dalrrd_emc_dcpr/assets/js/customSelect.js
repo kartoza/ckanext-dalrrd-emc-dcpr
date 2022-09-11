1e3ff5a2a59969bf1e17540a3ecf850bf675b9d1
Mohab <homab3@gmail.com>
2022-09-11 16:34:06 +0200

    adding extra maintenance field to display snippets

--- ckanext/dalrrd_emc_dcpr/assets/js/customSelect.js
+++ ckanext/dalrrd_emc_dcpr/assets/js/customSelect.js
@@ -1,4 +1,11 @@
 ckan.module("custom_select", function($){
+    /**
+     hide and show the other input field according
+     to selection result with maintenance field,
+     if the user selected "other" an input shows up
+     to include the other option from the user.
+     */
+
     var other_input_id = "maintenance_information-0-custom_other_choice_select"
     return{
         initialize:function(){
@@ -17,6 +24,30 @@ ckan.module("custom_select", function($){
             }
             else{
                 custom_input.show()
+            }
+        }
+    }
+})
+
+
+ckan.module("custom_select_display", function($){
+    /*
+        this works in the display field, by default
+        the display doesn't show the extra custom
+        input, it only shows "other".
+    */
+
+    return{
+        initialize: function(){
+            $.proxyAll(this,/_on/);
+            other_inputs_class = $(".dataset-label")
+            for(let other_input of other_inputs_class){
+                if(other_input.innerHTML.includes("Maintenance and update frequency")){
+                    other_field = other_input.nextElementSibling
+                    if(other_field.innerHTML.includes("other")){
+                        other_field.innerHTML = this.options.text
+                    }
+                }
             }
         }
     }
