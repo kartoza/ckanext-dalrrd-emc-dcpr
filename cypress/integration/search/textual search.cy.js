ca565ce92d09ff49043d153a1a10e969455f3324
Mohab <homab3@gmail.com>
2022-10-12 13:00:05 +0200

    adding tests for search from home page

--- cypress/integration/search/textual search.cy.js
+++ cypress/integration/search/textual search.cy.js
@@ -1,38 +1,45 @@
-// describe("login to emc local instance", ()=>{
-//     beforeEach(()=>{
-//         cy.visit("/") // http://localhost:5000 added as baseURL to cypress.json config file
-//     })
-//     it("logs in to EMC_dcpr system", ()=>{
-//         cy.get("[data-testid='nav_login']").click()
-//         .url().should('include', 'user/login')
-//         .get("#field-login")
-//         .type("admin")
-//         .should("have.value","admin")
-//         .get("#field-password")
-//         .type("12345678")
-//         .should("have.value","12345678")
-//         .get(".btn-primary").click() // change this
-//         .url().should('include', 'dashboard')
-//         .get("[data-testid='loggedin_user_icon']")
-//         .click()
-//         .get("[data-testid='logout']")
-//         .click()
-//         .url().should("include","/user")
-//         .get("[data-testid='nav_login']").contains("Log in")
-//     })
-
-// })
-
-
 describe("search for datasets via title", ()=>{
-    it("tests from the home page by text",()=>{
+    beforeEach(()=>{
         cy.visit("/")
+    })
+    it("finds a dataset from the home page by text",()=>{
         cy.get("[data-testid='home_dataset_search_input']")
         .type("test dataset no.1")
-        .url().should("include","/dataset/?q=dataset+no.1")
-        // cy.get("data-testid=['search_item_anchor']")
-        // .nextSibling.innerHTML.contains("test dataset")
-
-
+        .get("[data-testid='home_search_submit']").click()
+        .url().should("include","/dataset/?q=test+dataset+no.1")
+        .get("[data-testid='search_item_anchor']").next().eq(0).
+        should("have.text","dataset no 1")
     })
+
+    it("finds a dataset from the home page by abstract",()=>{
+        cy.get("[data-testid='home_dataset_search_input']")
+        .type("Default abstract for test")
+        .get("[data-testid='home_search_submit']").click()
+        //.url().should("include","/dataset/?q=test+dataset+no.2")
+        .get("[data-testid='search_item_anchor']").next().eq(0).
+        should("have.text","dataset no 1_v.4")
+    })
+
+    it("finds a dataset from the home page by doi",()=>{
+        cy.get("[data-testid='home_dataset_search_input']")
+        .type("10.15493/SARVA.CSAG.10000280")
+        .get("[data-testid='home_search_submit']").click()
+        //.url().should("include","/dataset/?q=test+dataset+no.2")
+        .get("[data-testid='search_item_anchor']").next().eq(0).
+        should("have.text","dataset no 1_v.4")
+    })
+
+    // it("finds a dataset from the search page by text",()=>{
+    //     setTimeout(()=>{},2000)
+    //     cy.visit("/dataset/")
+    //     cy.get("#field-giant-search")
+    //     .type("test dataset no.1")
+    //     .next().click()
+    //     .url().should("include","/dataset/?q=test+dataset+no.1")
+    //     .get("[data-testid='search_item_anchor']").next().eq(0).
+    //     should("have.text","dataset no 1")
+    // })
+
+
+
 })
