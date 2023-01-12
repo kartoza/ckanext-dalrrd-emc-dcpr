b5fc4a0bfa395fedb82b44bf3b80ab6c526e75a5
Mohab <homab3@gmail.com>
2023-01-12 22:02:31 +0200

    copy samweli error report

---
+++ ckanext/dalrrd_emc_dcpr/migration/dalrrd_emc_dcpr/versions/92499ccc1ad7_create_error_reports_tables.py
@@ -0,0 +1,24 @@
+"""create_error_reports_tables
+
+Revision ID: 92499ccc1ad7
+Revises: 9880e2b398a4
+Create Date: 2023-01-12 19:33:23.412331
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = "92499ccc1ad7"
+down_revision = "9880e2b398a4"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
